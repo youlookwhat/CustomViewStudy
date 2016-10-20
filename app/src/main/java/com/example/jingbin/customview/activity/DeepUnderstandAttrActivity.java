0d6e7a02af661cf3ea7efd937a4bb373a32cda15
youlookwhat <770413277@qq.com>
2016-10-20 12:25:47 +0800

    6.0 通过WebView增加深入理解Android中的自定义属性博文页面

---
+++ app/src/main/java/com/example/jingbin/customview/activity/DeepUnderstandAttrActivity.java
@@ -0,0 +1,15 @@
+package com.example.jingbin.customview.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.jingbin.customview.R;
+
+public class DeepUnderstandAttrActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deep_understand_att);
+    }
+}
